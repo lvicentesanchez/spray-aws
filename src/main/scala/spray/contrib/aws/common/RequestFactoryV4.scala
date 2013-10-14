e7e8989ae7dc9c392256c59442fb0f67abbf0d3a
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2013-10-15 00:12:37 +0100

    WIP - Improving performance of RequestBuilder.

--- src/main/scala/spray/contrib/aws/common/RequestFactoryV4.scala
+++ src/main/scala/spray/contrib/aws/common/RequestFactoryV4.scala
@@ -7,42 +7,47 @@ import java.net.URI
 import scala.collection.JavaConverters._
 import scala.language.implicitConversions
 import spray.http._
-import Uri.Query
-import HttpProtocols.`HTTP/1.1`
-import HttpMethods._
+import spray.http.HttpHeaders.RawHeader
+import spray.http.HttpProtocols.`HTTP/1.1`
+import spray.http.HttpMethods._
+import spray.http.Uri.Query
 
 trait RequestFactoryV4 { this: RequestFactory with Signer with Credentials ⇒
   def request[T](data: T)(implicit marshaller: Marshaller[Request[T], T]): HttpRequest = {
-    val awsReq = marshaller.marshall(data)
-    awsReq.setEndpoint(new URI("https://sqs.eu-west-1.amazonaws.com:443"))
-    awsReq.getHeaders.put("User-Agent", "spray-can/1.2-20131011")
-    //val body = awsReq.getContent.asInstanceOf[StringInputStream].getString
-    signature.sign(awsReq, credentials)
-    awsReq.getHeaders.remove("Host")
-    awsReq.getHeaders.remove("User-Agent")
-    awsReq.getHeaders.remove("Content-Length")
-    awsReq.getHeaders.remove("Content-Type")
-    val params = awsReq.getParameters().asScala
-    val uriPath: String = Option(awsReq.getResourcePath).getOrElse("/")
-    val request = HttpRequest(awsReq.getHttpMethod, Uri.from(port = 443, path = uriPath), headers(awsReq), HttpEntity(MediaTypes.`application/x-www-form-urlencoded`, Query(params.toSeq: _*).toString), `HTTP/1.1`)
-    request
+    val request = marshaller.marshall(data)
+    request.setEndpoint(new URI("https://sqs.eu-west-1.amazonaws.com:443"))
+    request.getHeaders.put("User-Agent", "spray-can/1.2-20131011")
+    //val body = request.getContent.asInstanceOf[StringInputStream].getString
+    signature.sign(request, credentials)
+    val params = request.getParameters().asScala
+    val uriPath: String = Option(request.getResourcePath).getOrElse("/")
+    val result = HttpRequest(methods(request.getHttpMethod), Uri.from(port = 443, path = uriPath), headers(request, illegalHeaders), HttpEntity(MediaTypes.`application/x-www-form-urlencoded`, Query(params.toSeq: _*).toString), `HTTP/1.1`)
+    result
   }
 
   private[this] val signature = SignatureProvider()
 
-  private[this] implicit def bridgeMethods(m: HttpMethodName): HttpMethod = m match {
-    case HttpMethodName.POST ⇒ POST
-    case HttpMethodName.GET ⇒ GET
-    case HttpMethodName.PUT ⇒ PUT
-    case HttpMethodName.DELETE ⇒ DELETE
-    case HttpMethodName.HEAD ⇒ HEAD
-  }
+  private[this] val methods: Map[HttpMethodName, HttpMethod] = Map(
+    HttpMethodName.POST -> POST,
+    HttpMethodName.GET -> GET,
+    HttpMethodName.DELETE -> DELETE,
+    HttpMethodName.HEAD -> HEAD
+  )
 
-  private[this] def headers(req: Request[_]): List[HttpHeader] = {
-    req.getHeaders.asScala.map {
-      case (k, v) ⇒
-        HttpHeaders.RawHeader(k, v)
-    }.toList
-  }
+  private[this] val illegalHeaders: Set[String] = Set(
+    "content-length",
+    "content-type",
+    "host",
+    "transfer-encoding",
+    "user-agent"
+  )
 
+  @inline
+  private[this] def headers(req: Request[_], exclude: Set[String]): List[HttpHeader] =
+    req.getHeaders.asScala.foldLeft(List.empty[HttpHeader]) {
+      case (acc, value @ (key, _)) if exclude(key.toLowerCase) == false ⇒ builder(value) :: acc
+    }
+
+  @inline
+  private[this] val builder: Tuple2[String, String] ⇒ HttpHeader = (RawHeader.apply _).tupled
 }
